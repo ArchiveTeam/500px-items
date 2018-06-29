f3e82d060f901cd620ef535e26ad138a8ba7d204
Arkiver2 <arkiver@protonmail.com>
2018-06-29 15:27:32 +0200

    Update to current situation

--- generate.py
+++ generate.py
@@ -5,6 +5,9 @@ import sys
 data = [[]]
 filenum = int(sys.argv[2])
 
+with open('processed_ids.txt', 'r') as f:
+    processed_ids = {l.strip() for l in f}
+
 def create_file(i, d):
     with open('{}_photos.txt'.format(str(i).zfill(4)), 'w') as f:
         f.write('\n'.join(['photos:{}'.format(';'.join(l)) for l in d]))
@@ -12,6 +15,9 @@ def create_file(i, d):
 with open(sys.argv[1], 'r') as f:
     for line in f:
         line = line.strip()
+        if line in processed_ids:
+            continue
+        processed_ids.add(line)
         if len(data[-1]) == 10:
             if len(data) == 50000:
                 create_file(filenum, data)
@@ -21,4 +27,7 @@ with open(sys.argv[1], 'r') as f:
         data[-1].append(line)
 
 create_file(filenum, data)
+
+with open('processed_ids.txt', 'w') as f:
+    f.write('\n'.join(processed_ids))
 
---
+++ original/get_ids.py
@@ -0,0 +1,22 @@
+import json
+import os
+import re
+import sys
+
+
+def main():
+    ids = set()
+    for name in os.listdir(sys.argv[1]):
+        path = os.path.join(sys.argv[1], name)
+        if not os.path.isfile(path):
+            continue
+        print(path)
+        with open(path, 'r') as f:
+            data = json.loads(re.search(r'(\{.+\})', f.read()).group(1))
+            ids |= {p['url'].split('/')[2] for p in data['photos']}
+    with open('{}_ids.txt'.format(sys.argv[1].rstrip('/')), 'w') as f:
+        f.write('\n'.join(sorted(ids)))
+
+if __name__ == '__main__':
+    main()
+
