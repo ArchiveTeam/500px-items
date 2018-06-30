eb84292ed8bd8f9a9feb08f8e3165cf278af2a99
Arkiver2 <arkiver@protonmail.com>
2018-06-30 04:17:31 +0200

    Created 'all' item list.

---
+++ generate_all.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    with open('08_all_{}_{}.txt'.format(start, end), 'w') as f:
+        f.write('\n'.join(['all:{}-{}'.format(i, i+999)
+                           for i in range(start, end, 1000)]))
+
+if __name__ == '__main__':
+    main()
+
