4505c026e3040dc06b4e65a78e9bc747e684511c
Arkiver2 <arkiver@protonmail.com>
2018-06-30 23:55:39 +0200

    0009 was added to tracker

--- generate_all.py
+++ generate_all.py
@@ -2,8 +2,8 @@ import sys
 
 
 def main():
-    start = int(sys.argv[1])
-    end = int(sys.argv[2])
+    start = 0
+    end = 265000000
     with open('0009_all_{}_{}_100_ids.txt'.format(start, end), 'w') as f:
         f.write('\n'.join(['all:{}-{}'.format(i, i+99)
                            for i in range(start, end, 100)]))
