2249bb898671f7910bc5bafd7d30ef4beed1668f
Arkiver2 <arkiver@protonmail.com>
2018-06-30 16:21:29 +0200

    Generate items with 100 IDs each.

--- generate_all.py
+++ generate_all.py
@@ -4,9 +4,9 @@ import sys
 def main():
     start = int(sys.argv[1])
     end = int(sys.argv[2])
-    with open('0008_all_{}_{}.txt'.format(start, end), 'w') as f:
-        f.write('\n'.join(['all:{}-{}'.format(i, i+999)
-                           for i in range(start, end, 1000)]))
+    with open('0009_all_{}_{}_100_ids.txt'.format(start, end), 'w') as f:
+        f.write('\n'.join(['all:{}-{}'.format(i, i+99)
+                           for i in range(start, end, 100)]))
 
 if __name__ == '__main__':
     main()
